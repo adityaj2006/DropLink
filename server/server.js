0a7196a58639be03e43b771ea042a0cad753f853
adityaj2006 <jaiswaladitya6002@gmail.com>
2026-05-09 01:14:36 +0530

    Restrict CORS to known origins

--- server/server.js
+++ server/server.js
@@ -6,10 +6,12 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "https://drop-link-p2p.vercel.app"
+    ],
     methods: ["GET", "POST"],
   },
 });
